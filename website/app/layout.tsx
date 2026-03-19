34277e1072469597b44189c232be73842173f95d
Moshe Atlow <moshe@atlow.co.il>
2026-03-19 22:03:21 +0200

    revert

--- website/app/layout.tsx
+++ website/app/layout.tsx
@@ -29,6 +29,9 @@ export default function RootLayout({
       lang="en"
       className={`${heebo.variable} ${jetbrainsMono.variable}`}
     >
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
       <body className="min-h-screen antialiased">
         {children}
       </body>
